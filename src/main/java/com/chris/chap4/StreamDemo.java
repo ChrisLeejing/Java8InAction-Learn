2f6e36bf471c42d1be56036c45c6d5bbfdb2bdbc
ChrisLeejing <chris_lee_jing@163.com>
2019-06-17 16:40:43 +0800

    [ADD]: 1. Add chap4 4.5, true finished the chap4.

--- src/main/java/com/chris/chap4/StreamDemo.java
+++ src/main/java/com/chris/chap4/StreamDemo.java
@@ -149,5 +149,14 @@ public class StreamDemo {
             map dish name: chicken
             count: 3
          */
+
+        // 4.5. Summary
+        // Here are some key concepts to take away from this chapter:
+        // * A stream is a sequence of elements from a source that supports data processing operations.
+        // * Streams make use of internal iteration: the iteration is abstracted away through operations such as filter, map, and sorted.
+        // * There are two types of stream operations: intermediate and terminal operations.
+        // * Intermediate operations such as filter and map return a stream and can be chained together. They’re used to set up a pipeline of operations but don’t produce any result.
+        // * Terminal operations such as forEach and count return a non-stream value and process a stream pipeline to return a result.
+        // * The elements of a stream are computed on demand.
     }
 }
